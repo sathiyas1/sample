cb6b064e45c1e14bf6e6ec871d67d5a362465726
Sathiya <sathyaseelans.22cse@kongu.edu>
2024-12-28 12:00:54 +0530

    second command

---
+++ operator.js
@@ -0,0 +1,32 @@
+//add\
+var a=10
+var b=10
+console.log(a+b)
+
+//logical 
+var a = 10
+var b = 30
+console.log(a&b)
+//
+var a = 10
+var b =5
+if(a==b)
+{
+    console.log("true")
+}
+else if(a==b)
+{
+    console.log("ture")
+}
+else if(a!=b)
+    {
+        console.log("ture")
+    }
+else if((a==b)&&(a!=b))
+{
+    console.log("ture")
+}    
+else
+{
+    console.log("true")
+}
